b73288208d9b3071d0614acedbcee5b7572e730a
mark-mammothc <mark@mammothcreative.com.au>
2025-03-22 12:13:58 +1000

    add shot timer

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
-import pygame.draw
+import pygame
+import random
+from constants import *
 from circleshape import CircleShape
 
 
@@ -7,8 +9,25 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
